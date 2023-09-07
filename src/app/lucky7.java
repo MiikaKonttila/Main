e68fdc51e9fdc012b4108f5083fa75cffe2978e5
MiikaKonttila <miika20113@student.hamk.fi>
2023-09-07 09:41:08 +0300

    Viimeistelin pelin
    Laitoin peliin if lauseet jotka katsoo mitkä numerot sait ja voititko rahaa

--- src/app/lucky7.java
+++ src/app/lucky7.java
@@ -20,14 +20,41 @@ while (rahat > 0) {
 	n1 = random.nextInt(10) + 1;
 	n2 = random.nextInt(10) + 1;
 	n2 = random.nextInt(10) + 1;
+	if(n1 == 7 && n2 == 7 && n3 ==7) {
+		rahat = rahat + 10;
+		System.out.println("Sait numerot " + n1 + ", " + n2 + " ja " + n3 + ". Voitit pelin ja sait 10 rahaa");
+		System.out.println("Rahaa jäljellä " + rahat);
+	}
+	else if(n1 == 7 && n2 == 7 || n2 == 7 && n3 == 7 || n1 == 7 && n3 == 7) {
+		rahat = rahat + 5;
+		System.out.println("Sait numerot " + n1 + ", " + n2 + " ja " + n3 + ". Voitit pelin ja sait 5 rahaa");
+		System.out.println("Rahaa jäljellä " + rahat);
+	}
+	else if(n1 == 7 || n2 == 7 || n3 == 7) {
+		rahat = rahat + 3;
+		System.out.println("Sait numerot " + n1 + ", " + n2 + " ja " + n3 + ". Voitit pelin ja sait 3 rahaa");
+		System.out.println("Rahaa jäljellä " + rahat);
+	}else {
+		System.out.println("Sait numerot " + n1 + ", " + n2 + " ja " + n3 + ". Hävisit pelin");
+		System.out.println("Rahaa jäljellä " + rahat);
+	}
+	System.out.println("Haluatko pelata uudestaan?");
+	System.out.println("Jos haluat pelata, paina Enter");
+	System.out.println("Jos et halua pelata kirjoita en");
+	String vastaus = in.nextLine();
+	if(vastaus.equals("en")) {
+		System.out.println("Peli päättyi, Sinulle jäi " + rahat + " euroa");
+		System.exit(0);		
+		
+	}
 	
+		
+	}
 }
 	
 
 	    
 	    	
 	    	
-	   	
-	    }    	       
-	   }       			
+	   	 }    	            			
 
